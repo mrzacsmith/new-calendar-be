37cdf5503adfe4a0b36e253b74912ac998f3bc9d
Zac Smith <zrs3141592@gmail.com>
2020-09-11 16:27:34 -0600

    update model

--- models/Days.js
+++ models/Days.js
@@ -5,7 +5,7 @@ const DaySchema = new mongoose.Schema(
     dayOYear: { type: String, required: true },
     seasonIn: { type: String, required: true },
     monthIn: { type: String, required: true },
-    newDate: { type: Number, required: true },
+    newDate: { type: Number, required: true, unique: true },
     holiday: { type: String },
   },
   {
