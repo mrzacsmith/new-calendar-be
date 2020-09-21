a41071050c38e6f4e785bdef16060ad8f175d73f
Zac Smith <zrs3141592@gmail.com>
2020-09-20 19:10:50 -0600

    work on script to create calendar

--- days_script/creator.js
+++ days_script/creator.js
@@ -1,24 +1,39 @@
+const { model } = require('mongoose')
+
 const daysArr = [1, 'winter', 'mid']
 const keysArr = ['_id', 'seasonIn', 'monthIn']
-const daysObj = []
+const daysObj = [{ _id: 0 }]
 
-const newCalendar = () => {
-  const DAYS = 150
+const newCalendar = (DAYS) => {
+  // const DAYS = 150
 
-  for (let day = 1; day <= DAYS; day++) {
-    daysObj[day] = {
-      _id: day,
-      seasonIn: 'season',
-      monthIn: day,
+  for (let id = 32; id <= DAYS; id++) {
+    let season = ''
+    let day = 1
+    if (id < 74) {
+      season = 'winter'
+    } else if (id < 147) {
+      season = 'spring'
+    } else if (id < 219) {
+      season = 'summer'
+    } else if (id < 292) {
+      season = 'autumn'
+    } else {
+      season = 'fall'
+    }
+
+    dayW = id % 74 == 0 ? 1 : id % 74
+
+    daysObj[id] = {
+      _id: id,
+      day: dayW,
+      seasonIn: season,
+      monthIn: dayW < 37 ? 'early' : dayW == 37 ? 'mid' : 'late',
     }
   }
   return daysObj
 }
 
-const sample = {
-  _id: 1,
-  seasonIn: 'winter',
-  monthIn: 'mid',
-}
+console.log(newCalendar(120))
 
-console.log(newCalendar())
+module.exports = daysObj
